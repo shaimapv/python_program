f82895f227c1ab8a9f468d94196be335383c8e7a
shaimapv <161370041+shaimapv@users.noreply.github.com>
2025-06-06 17:53:53 +0530

    Update operators.py

--- operators.py
+++ operators.py
@@ -75,3 +75,99 @@ elif choice == 6 :
     special()
 else:
     print("invalid choice")
+
+
+""" output:
+PS C:\training\2nd task> python operators.py 
+1. Arithmetic Operations 
+2. Assignment Operations 
+3. Comparison Operations 
+4.Logical Operations 
+5. Bitwise Operations
+6. Special Operations
+enter your choice : 1
+enter first number : 5
+enter second number : 6
+num1 + num2 = 11
+num1 - num2 = -1
+num1 * num2 = 30
+num1 / num2 = 0.8333333333333334
+num1 % num2 = 5
+num1 // num2 = 0
+num1 ** num2 = 15625
+PS C:\training\2nd task> python operators.py
+1. Arithmetic Operations 
+2. Assignment Operations
+3. Comparison Operations
+4.Logical Operations
+5. Bitwise Operations
+6. Special Operations
+enter your choice : 2
+enter a number to perform assignment operations : 5
+num1 = 5
+num1 += 2 -> 7
+num1 -= 3 -> 4
+num1 *= 4 -> 16
+num1 /= 2 -> 8.0
+num1 %= 3 -> 2.0
+num1 //= 2 -> 1.0
+num1 **= 3 -> 1.0
+PS C:\training\2nd task> python operators.py
+1. Arithmetic Operations 
+2. Assignment Operations
+3. Comparison Operations
+4.Logical Operations
+5. Bitwise Operations
+6. Special Operations
+enter your choice : 3
+enter first number : 4
+enter second number : 2
+num1 == num2: False
+num1 != num2: True
+num1 > num2: True
+num1 < num2: False
+num1 >= num2: True
+num1 <= num2: False
+PS C:\training\2nd task> python operators.py
+1. Arithmetic Operations 
+2. Assignment Operations
+3. Comparison Operations
+4.Logical Operations
+5. Bitwise Operations
+6. Special Operations
+enter your choice : 4
+enter first boolen value : True
+enter second boolean value : True
+num1 and num2: True
+num1 or num2: True
+not num1: False
+PS C:\training\2nd task> python operators.py
+1. Arithmetic Operations 
+2. Assignment Operations
+3. Comparison Operations
+4.Logical Operations
+5. Bitwise Operations
+6. Special Operations
+enter your choice : 5
+enter first number : 5
+enter second number : 2
+num1 & num2: 0
+num1 | num2: 7
+num1 ^ num2: 7
+~num1: -6
+num1 << 1: 10
+num1 >> 1: 2
+PS C:\training\2nd task> python operators.py
+1. Arithmetic Operations 
+2. Assignment Operations
+3. Comparison Operations
+4.Logical Operations
+5. Bitwise Operations
+6. Special Operations
+enter your choice : 6
+x is z: True
+x is y: False
+x == y: True
+x is not y: True
+1 in x: True
+4 not in x: True """
