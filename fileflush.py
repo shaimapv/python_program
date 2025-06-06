76356b7b4ae21a84d4d89ea021b96dc7564a2065
shaimapv <161370041+shaimapv@users.noreply.github.com>
2025-06-06 17:56:16 +0530

    Update fileflush.py

--- fileflush.py
+++ fileflush.py
@@ -8,3 +8,19 @@ for i in range(5):
 print("demonstrate working of file and flush",flush=True )
 
 print("hello world",file=file_1)
+
+
+
+"""  output:
+PS C:\training\2nd task> python fileflush.py
+demonstrate working of file and flush
+
+file.txt:
+logging event 1
+logging event 2
+logging event 3
+logging event 4
+logging event 5
+hello world
+
+"""
