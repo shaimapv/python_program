426f1e2079936a3dbff0a7b4d74793051bb3ead3
shaimapv <161370041+shaimapv@users.noreply.github.com>
2025-06-11 12:22:48 +0530

    Add files via upload

---
+++ left_right.py
@@ -0,0 +1,16 @@
+print("----- left shift and right shift -----")
+num1 = int(input("enter a number : "))
+num2 = int(input("enter a number of positions to shift :"))
+print("left shift is ",num1<<num2)
+print("right shift is ",num1>>num2)
+
+
+"""output:
+PS C:\training\4th task> python left_right.py
+----- left shift and right shift -----
+enter a number : 5
+enter a number of positions to shift :2
+left shift is  20
+right shift is  1
+
+"""
---
+++ xor.py
@@ -0,0 +1,16 @@
+
+print("----- XOR -----")
+num1 = int(input("enter a number : "))
+num2 = int(input("enter a number : "))
+
+XOR_num = num1^num2
+
+print("XOR of ",num1 ," and ",num2 ," is ",XOR_num)
+
+"""output:
+PS C:\training\4th task> python xor.py
+----- XOR -----
+enter a number : 1
+enter a number : 2
+XOR of  1  and  2  is  3
+"""
