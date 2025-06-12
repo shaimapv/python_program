752eb10f605c72bdde839bf67a04d9e883e07cda
shaimapv <161370041+shaimapv@users.noreply.github.com>
2025-06-12 16:08:18 +0530

    Update quiz_game.py

--- quiz_game.py
+++ quiz_game.py
@@ -57,6 +57,102 @@ while(True):
         print("Thanks for playing!")
         break
 
+"""
+Output:
+PS C:\training\5th task> python quiz_game.py
+Wlcome to quiz game 
+enter your name : adam
+hello adam , let's start the game 
+
+---level 1---
+
+ what is capital of india ?
+A.Mumbai
+B.Delhi
+C.Kolkata
+D.Lucknow
+enter answer(A/B/C/D) :B
+answer is correct 
+
+ 2+2=?
+A.5
+B.4
+C.3
+D.6
+enter answer(A/B/C/D) :B
+answer is correct 
+
+ Which is a programming language?
+A.python
+B.snake
+C.django
+D.http
+enter answer(A/B/C/D) :A
+answer is correct 
+score is 3
+
+Excellent
+
+---level 2---
+
+ which gas is most abundant in earth's atmosphere ?
+A.Oxygen
+B.Nitrogen
+C.Hydrogen
+D.Carbon Dioxide
+enter answer(A/B/C/D) :B
+answer is correct
+
+ In which year did india become a republic ?
+A.1947
+B.1950
+C.1952
+D.1949
+enter answer(A/B/C/D) :B
+answer is correct
+
+ which city is known as 'city of canals' ?
+A.Amsterdam
+B.Bangkok
+C.Bruges
+D.Venice
+enter answer(A/B/C/D) :D
+answer is correct
+score is 3
+
+Excellent
+
+---level 3---
+
+ In 2023, which tech company introduced the AI tool named "Bard" as a competitor to ChatGPT ?
+A.Microsoft
+B.Google
+C.Amazon
+D.Metaw
+enter answer(A/B/C/D) :B
+answer is correct
+
+ which vitamin is primarily obtained from sunlight ?
+A.vitamin A
+B.vitamin B12
+C.vitamin C
+D.vitamin D
+enter answer(A/B/C/D) :D
+answer is correct
+
+ RAM is a type of?
+A. Storage
+B. Input
+C. Output
+D. Memory
+enter answer(A/B/C/D) :d
+answer is correct
+score is 3
+
+Excellent
+Congratulation , you completed all levels
+Do you want to play again? (y/n): n
+Thanks for playing!"""
 
 
 
