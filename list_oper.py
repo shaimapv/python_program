0916d2ae799ebbe65b7d3eb71f51f53ff9af58ec
shaimapv <161370041+shaimapv@users.noreply.github.com>
2025-06-13 18:37:51 +0530

    Update list_oper.py

--- list_oper.py
+++ list_oper.py
@@ -23,3 +23,36 @@ print("reversed list is : ",list)
 print("length os list is : ",len(list))
 list.clear()
 print("after clearing : ",list)
+
+"""
+Output:
+PS C:\training\6th task> python list_oper.py
+original list is  [1, 2, 3, 4, 5]
+first element is  1
+last element is  5
+updated list is  [1, 25, 3, 4, 5]
+after append list is  [1, 25, 3, 4, 5, 9]
+after inserting in to index 2 list is  [1, 25, 30, 3, 4, 5, 9]
+after extend :  [1, 25, 30, 3, 4, 5, 9, 11, 12]
+updated list is  [1, 25, 3, 4, 5]
+after append list is  [1, 25, 3, 4, 5, 9]
+after inserting in to index 2 list is  [1, 25, 30, 3, 4, 5, 9]
+after extend :  [1, 25, 30, 3, 4, 5, 9, 11, 12]
+after inserting in to index 2 list is  [1, 25, 30, 3, 4, 5, 9]
+after extend :  [1, 25, 30, 3, 4, 5, 9, 11, 12]
+after removing 4 :  [1, 25, 30, 3, 5, 9, 11, 12]
+after extend :  [1, 25, 30, 3, 4, 5, 9, 11, 12]
+after removing 4 :  [1, 25, 30, 3, 5, 9, 11, 12]
+popped item :  12
+after removing 4 :  [1, 25, 30, 3, 5, 9, 11, 12]
+popped item :  12
+popped item :  12
+after pop :  [1, 25, 30, 3, 5, 9, 11]
+index of 3 is :  3
+after pop :  [1, 25, 30, 3, 5, 9, 11]
+index of 3 is :  3
+index of 3 is :  3
+sorted list is :  [1, 3, 5, 9, 11, 25, 30]
+reversed list is :  [30, 25, 11, 9, 5, 3, 1]
+length os list is :  7
+after clearing :  []"""
